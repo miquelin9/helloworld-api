ae11150ad16e1c48604d4c6137f7a72316718f45
Miquel Jordà Lopez <miquelin9@gmail.com>
2021-03-16 16:12:15 +0100

    test n4

--- src/main/java/com/modeling/HelloController.java
+++ src/main/java/com/modeling/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello World, i'm the change n3!";
+        return "Hello World, i'm the change n4!";
     }
 }
