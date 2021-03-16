3d564a215b0b91f773765f94cd8563c5f3a447a4
Miquel Jordà Lopez <miquelin9@gmail.com>
2021-03-16 14:57:51 +0100

    Test renaming fix

--- src/test/java/com/modeling/NativeExampleResourceIT.java
+++ src/test/java/com/modeling/NativeExampleResourceIT.java
@@ -3,7 +3,7 @@ package com.modeling;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
+public class NativeExampleResourceIT extends HelloControllerTest {
 
     // Execute the same tests but in native mode.
 }
